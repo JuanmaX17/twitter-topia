d202f7de437ab63a7f3036e6cdf111392ed06b05
Karen <karen2090@gmail.com>
2023-04-19 12:09:53 -0500

    squash commits

---
+++ src/pages/api/tweets.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const tweets = await prisma.tweet.findMany();
+
+  res.status(200).json(tweets);
+}
